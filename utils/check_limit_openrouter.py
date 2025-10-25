bacf3e4b6e3addf294e5205a361149b6fc81118a
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2025-10-25 10:46:16 -0300

    Ajusta check limit openrouter

--- utils/check_limit_openrouter.py
+++ utils/check_limit_openrouter.py
@@ -3,7 +3,9 @@ import json
 response = requests.get(
   url="https://openrouter.ai/api/v1/key",
   headers={
-    "Authorization": f"Bearer <seu_token_aqui>"
+    "Authorization": f"Bearer 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"
   }
 )
-print(json.dumps(response.json(), indent=2))
+data = response.json()
+data["data"]["usage"]
+print(f'${data["data"]["usage"]:.2f} used today.')
